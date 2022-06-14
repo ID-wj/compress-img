26dc264b41da03449227224cfe6b77b864285000
liwj <773577089@qq.com>
2022-06-14 11:10:09 +0800

    add: 拖拽上传

--- src/components/compress-img/index.vue
+++ src/components/compress-img/index.vue
@@ -1,31 +1,61 @@
 <template>
   <div class="ci-box">
-    <div class="drag-box" @drag="handleDrag"></div>
+    <div id="dragBox" class="drag-box">
+      <ul>
+        <li v-for="item of files" :key="item.lastModified">{{ item.name }}</li>
+      </ul>
+    </div>
+    <div class="upload-button">上传</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeUnmount, onMounted } from "vue";
+import { defineComponent, onBeforeUnmount, onMounted, ref } from "vue";
 export default defineComponent({
   setup() {
     let count = 0;
+    let files = ref<FileList>();
     function handleDrag(e: DragEvent) {
+      e.stopPropagation();
       e.preventDefault();
-      console.log(e);
+      return false;
+    }
+    function handleDrop(e: DragEvent) {
+      e.stopPropagation();
+      e.preventDefault();
+      if (e?.dataTransfer?.files) {
+        files.value = e?.dataTransfer?.files;
+      } else {
+        files.value = undefined;
+      }
+      return false;
     }
     function documentDrag(e: DragEvent) {
       e.stopPropagation();
       e.preventDefault();
+      return false;
     }
     onMounted(() => {
-      window.addEventListener("dragover", documentDrag);
-      window.addEventListener("drag", documentDrag);
+      document.addEventListener("dragover", documentDrag, false);
+      document.addEventListener("drop", documentDrag, false);
+      document.addEventListener("dragenter", documentDrag, false);
+
+      const dragBox = document.getElementById("dragBox");
+      dragBox?.addEventListener("dragover", handleDrag, false);
+      dragBox?.addEventListener("drop", handleDrop, false);
+      dragBox?.addEventListener("dragenter", handleDrag, false);
     });
     onBeforeUnmount(() => {
-      window.removeEventListener("dragover", documentDrag);
-      window.removeEventListener("drag", documentDrag);
+      document.removeEventListener("dragover", documentDrag, false);
+      document.removeEventListener("drop", documentDrag, false);
+      document.removeEventListener("dragenter", documentDrag, false);
+
+      const dragBox = document.getElementById("dragBox");
+      dragBox?.removeEventListener("dragover", handleDrag, false);
+      dragBox?.removeEventListener("drop", handleDrop, false);
+      dragBox?.removeEventListener("dragenter", handleDrag, false);
     });
-    return { count, handleDrag };
+    return { count, handleDrag, files };
   },
 });
 </script>
@@ -37,6 +67,15 @@ export default defineComponent({
     height: 500px;
     border: 2px dashed burlywood;
     border-radius: 4px;
+  }
+  .upload-button {
+    display: inline-block;
+    padding: 10px;
+    // border: 1px solid blue;
+    background: burlywood;
+    color: #fff;
+    border-radius: 4px;
+    margin-top: 10px;
   }
 }
 </style>
